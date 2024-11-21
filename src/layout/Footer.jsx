bffda311aebffa2076c74fda44fd6f2b688f723d
Eli RoCas <elir_94@hotmail.com>
2024-11-21 16:14:45 -0500

    add: estilo del footer y componentes básicos

---
+++ src/layout/Footer.jsx
@@ -0,0 +1,13 @@
+import "./footer.css"
+
+const Footer = () => {
+    return (
+        <footer className="footerContainer">
+            <p> © MD - KeyCode - 2024</p>
+            <p>HackBike</p>
+            <p>All Rights Reserved.</p>
+        </footer>
+    );
+};
+
+export default Footer;
